806068694500e51d5331e80aa3b7e72d02689a90
Jordan Farelli <jordanfarelli@gmail.com>
2023-04-11 13:03:25 -0600

    apiCalls carryover

---
+++ src/apiCalls.js
@@ -0,0 +1,16 @@
+const fetchBaconData = async () => {
+	try {
+		const response = await fetch('http://localhost:8000/');
+		if (!response.ok) {
+			throw new Error(
+				'Something went wrong. Please try again!'
+			);
+		}
+		const json = await response.json();
+		return json;
+	} catch (error) {
+		console.log('ERROR!!!', error);
+	}
+};
+
+export default fetchBaconData;
