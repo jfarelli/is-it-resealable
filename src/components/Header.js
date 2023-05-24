2069d9f06ad1a8eecaddcce1680af9e9df48ac62
Jordan Farelli <jordanfarelli@gmail.com>
2023-05-23 21:08:10 -0600

    Created Header test file and wrote unit tests

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 const Header = ({ scrolled }) => {
 	return (
 		<div
+			data-testid="header-element"
 			className={`bg-[#F9BB38] text-[#9B4428] h-16 z-10 flex items-center justify-center drop-shadow-2xl fixed top-0 left-0 right-0 font-bold ${
 				scrolled ? 'shadow-sm shadow-gray-700' : ''
 			}`}
---
+++ src/tests/Header.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from '../components/Header';
+
+describe('Header', () => {
+	it('renders the header with default styles when not scrolled', () => {
+		render(
+			<BrowserRouter>
+				<Header scrolled={false} />
+			</BrowserRouter>
+		);
+
+		const headerElement = screen.getByRole('heading');
+		const linkElement = screen.getByRole('link', {
+			name: /is your bacon resealable\?\?\?/i,
+		});
+
+		expect(headerElement).toBeInTheDocument();
+		expect(headerElement).toHaveClass(
+			'text-center bg-[#F9BB38] text-2xl sm:text-4xl hover:cursor-pointer'
+		);
+		expect(headerElement).not.toHaveClass('shadow-sm shadow-gray-700');
+		expect(linkElement).toBeInTheDocument();
+		expect(linkElement).toHaveAttribute('href', '/');
+
+		fireEvent.mouseEnter(linkElement);
+
+		expect(window.location.pathname).toBe('/');
+	});
+
+	it('renders the header with additional styles when scrolled', () => {
+		render(
+			<BrowserRouter>
+				<Header scrolled={true} />
+			</BrowserRouter>
+		);
+
+		const headerComponent = screen.getByTestId('header-element');
+		const headerElement = screen.getByRole('heading');
+		const linkElement = screen.getByRole('link', {
+			name: /is your bacon resealable\?\?\?/i,
+		});
+
+		expect(headerComponent).toBeInTheDocument();
+		expect(headerComponent).toHaveClass(
+			'bg-[#F9BB38] text-[#9B4428] h-16 z-10 flex items-center justify-center drop-shadow-2xl fixed top-0 left-0 right-0 font-bold shadow-sm shadow-gray-700'
+		);
+		expect(headerElement).toHaveClass(
+			'text-center bg-[#F9BB38] text-2xl sm:text-4xl hover:cursor-pointer'
+		);
+		expect(headerElement).toBeInTheDocument();
+		expect(linkElement).toBeInTheDocument();
+		expect(linkElement).toHaveAttribute('href', '/');
+
+		fireEvent.mouseEnter(linkElement);
+
+		expect(window.location.pathname).toBe('/');
+	});
+});
